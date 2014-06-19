b524443fb9b653be00a672d43167ed0db19c2d75
ltlollo <ltlollo.it@gmail.com>
2014-06-19 14:44:27 +0200

    s

--- DebPack.cmake
+++ DebPack.cmake
@@ -6,6 +6,10 @@ SET(CPACK_GENERATOR "DEB")
 set(CPACK_DEBIAN_PACKAGE_MAINTAINER "ltlollo")
 set(CPACK_PACKAGE_CONTACT "example@example.example")
 set(CPACK_SET_DESTDIR on)
-set(CPACK_DEBIAN_PACKAGE_DEPENDS " libsfml-graphics2 (>= 2.1+dfsg2-1), libsfml-system2 (>= 2.1+dfsg2-1), libsfml-window2 (>= 2.1+dfsg2-1), fonts-dejavu (>= 2.34-1)")
+set(CPACK_DEBIAN_PACKAGE_DEPENDS
+    "libsfml-graphics2 (>= 2.1+dfsg2-1),
+    libsfml-system2 (>= 2.1+dfsg2-1),
+    libsfml-window2 (>= 2.1+dfsg2-1),
+    fonts-dejavu (>= 2.34-1)")
 
 include(CPack)
